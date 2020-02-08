3e1d6baf6157b65b7b966df3839784dc9f722162
AHoltzapple <34113841+AHoltzapple@users.noreply.github.com>
2020-02-08 15:38:17 -0500

    Update hnanalysis.py

--- hnanalysis.py
+++ hnanalysis.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
+### This code removes posts with 0 comments then samples 20,000 for analysis###
 
+### Import source data file
 import csv
 
 hn_full = []
@@ -17,8 +15,8 @@ with open('hacker_news_source.csv', encoding='utf8') as csv_file:
 headers = hn_full[0]
 hn_full = hn_full[1:]
 
-print(headers)
 
+### Remove posts with 0 comments
 hn_comments = []
 
 for post in hn_full:
@@ -26,14 +24,13 @@ for post in hn_full:
     if comments > 0:
         hn_comments.append(post)
 
-print(len(hn_comments))
 
+### Randomly sample 20,000 posts for analysis
 from random import sample
-
 hn = sample(hn_comments,20000)
 
-print(len(hn))
 
+### Write sampled data into new file for analysis
 with open('hacker_news.csv', 'w', newline='', encoding='utf8') as sample_file:
     write_file = csv.writer(sample_file, delimiter=',')
     write_file.writerow(headers)
